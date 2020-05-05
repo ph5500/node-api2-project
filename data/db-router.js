80bd05242624636ee0b091ce95de370068ef3b7d
ph5500 <philfives@gmail.com>
2020-05-05 17:02:18 -0600

    Close to mvp, need to figure out posting data

---
+++ data/db-router.js
@@ -0,0 +1,137 @@
+const express = require("express");
+
+const Posts = require("./db.js");
+
+const router = express.Router();
+
+//POST requests
+
+router.post("/", (req, res) => {
+    Posts.insert(req.body)
+        .then(post => {
+            if (!req.body.title || !req.body.contents) {
+                res.status(400).json({
+                    errorMessage: "Please provide title and contents for the post."
+                });
+            } else {
+                res.status(201).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                errorMessage: "There was an error while saving the post to the database"
+            });
+        });
+});
+
+router.post("/:id/comments", (req, res) => {
+    const { id } = req.params;
+    const comment = { ...req.body, post_id: id }
+    if (!req.body.text) {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide text for the comment" })
+    } else {
+        Posts.insertComment(comment)
+            .then(inserted => {
+                if (!inserted) {
+                    res.status(404).json({
+                        errorMessage: "The post with the specified ID does not exist"
+                    });
+                } else {
+                    res.status(201).json(inserted);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "There was an error while saving the comment to the database."
+                });
+            });
+    }
+});
+
+//GET requests
+
+router.get("/", (req, res) => {
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                errorMessage: "The posts information could not be retrieved."
+            });
+        });
+});
+
+router.get("/:id", (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post <= 0) {
+                res.status(404).json({
+                    errorMessage: "The post with the specified ID does not exist."
+                });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res
+                .status(500)
+                .json({
+                    errorMessage: "The post information could not be retrieved."
+                });
+        });
+});
+
+//DELETE requests
+router.delete("/:id", (req, res) => {
+    Posts.remove(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({
+                    errorMessage: "The post with the specified ID does not exist."
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "The post could not be removed." });
+        });
+});
+
+//PUT requests
+
+router.put("/:id", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        });
+    } else {
+        Posts.update(req.params.id, req.body)
+            .then(post => {
+                if (post) {
+                    res.status(200).json(post);
+                } else {
+                    res.status(404).json({
+                        errorMessage: "The post with the specified ID does not exist."
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    errorMessage: "The post information could not be modified."
+                });
+            });
+    }
+});
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -7,3 +7,24 @@ const server = express();
 server.use(express.json());
 
 server.use("/api/posts", dbRouter);
+
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Deployed API</h2>`)
+})
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+})
+
+
+
+// server.use('/api/posts', dbRouter);
+// server.listen(5000, () => {
+//     console.log(`\n*** Server Running on http://localhost:5000 ***\n`);
+// })
+
+
+
